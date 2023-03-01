bb08406ed1738ab6c09a0cbe86c302dc1ca779a4
shadowlessScript <munyasia225@gmail.com>
2023-03-01 19:48:52 +0300

    Survived a merge disaster

--- LIB/LIB/settings.py
+++ LIB/LIB/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-c7pep_6q!jvdt3lb1+!1l5#f6ji90a0v&2(_)22fz7&yx#h17l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
